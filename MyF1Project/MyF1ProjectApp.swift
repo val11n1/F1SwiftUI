b04b6b51028333295dac6f9e20717786d44a4d17
val <vtrusov@team.amocrm.com>
2024-08-24 20:09:25 +0300

    Initial Commit

---
+++ MyF1Project/MyF1ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyF1ProjectApp.swift
+//  MyF1Project
+//
+//  Created by Valeriy Trusov on 24.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct MyF1ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
